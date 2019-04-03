4a4a0e691d7fc11f7efa809a088eaf9b284e3eba
mst123 <624952546@qq.com>
2019-04-03 09:48:46 +0800

    no message

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -2,16 +2,19 @@
     <div>
         <home-header></home-header>
         <home-swiper></home-swiper>
+        <home-icons></home-icons>
     </div>
 </template>
 <script>
 import HomeHeader from './components/header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/iconNav'
 export default {
     name: 'Home',
     components: {
         HomeHeader: HomeHeader,
-        HomeSwiper: HomeSwiper
+        HomeSwiper: HomeSwiper,
+        HomeIcons: HomeIcons
     }
 }
 </script>
---
+++ src/pages/Home/components/iconNav.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="icons">
+        <div class="icon">
+            <div class="img-box">
+                 <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1554211021057&di=3c56e4343daad044c801adec1632592c&imgtype=0&src=http%3A%2F%2Fcdn6.haitou.cc%2Fcompany%2Flogo%2F62%2F5262_7e20.png%3Fv%3D1460098902" alt="" >
+            </div>
+           
+            <p class="nav-des">热门景点</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'HomeIcons'
+}
+</script>
+<style scoped>
+    .icons{
+        height: 0;
+        padding-bottom: 50%;
+        background: green;
+    }
+    .icons .icon{
+        width: 25%;
+        height: 0;
+        padding-bottom: 25%;
+        background: red;
+        overflow: hidden;
+        position: relative;
+        
+    }
+    .icons .icon .img-box{
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0.44rem;
+        box-sizing: border-box;
+        padding: 0.1rem;
+    }
+    .icons .icon .img-box img{
+        height: 100%;
+        width: 100%;
+        display: block;
+        margin: 0 auto;
+    }
+</style>
