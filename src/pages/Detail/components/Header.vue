abf2eb6fe7fe47d2260db656e91c20a6b60a5691
mst123 <624952546@qq.com>
2019-04-11 17:14:23 +0800

    mst

---
+++ src/pages/Detail/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<div class="header-abs"></div>
+        <div class="header-fixed"></div>
+	</div>
+</template>
+<script>
+export default {
+	name: 'DetailHeader',
+	components: {
+		
+	}
+}
+</script>
+<style scoped>
+    .header-abs{
+        position: absolute;
+        left: 0.2rem;
+        top: 0.2rem;
+        width: 0.8rem;
+        height: 0.8rem;
+        border-radius: 0.4rem;
+        background: rgba(0, 0, 0, 0.8)
+    }
+</style>
