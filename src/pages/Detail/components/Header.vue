6d02d73725d261892736960d0d5996286eb05c67
mst123 <624952546@qq.com>
2019-04-20 19:48:27 +0800

    详情页

--- src/pages/Detail/components/Header.vue
+++ src/pages/Detail/components/Header.vue
@@ -1,14 +1,18 @@
 <template>
 	<div>
-		<div class="header-abs"></div>
-        <div class="header-fixed"></div>
+		<div class="header-abs">
+			<span class="iconfont icon-return">&#xe624;</span>
+		</div>
+        <div class="header-fixed">
+
+		</div>
 	</div>
 </template>
 <script>
 export default {
 	name: 'DetailHeader',
 	components: {
-		
+
 	}
 }
 </script>
@@ -20,6 +24,11 @@ export default {
         width: 0.8rem;
         height: 0.8rem;
         border-radius: 0.4rem;
-        background: rgba(0, 0, 0, 0.8)
+        background: rgba(0, 0, 0, 0.8);
+		line-height: 0.8rem;
     }
+	.icon-return{
+		color: #ffffff;
+		font-size: 0.4rem;
+	}
 </style>
